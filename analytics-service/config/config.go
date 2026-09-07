f21b8910c309c288e18f8e6fb2f248e65489ad8a
Симоненко Ярослав Александрович <simonenko@noise.aero>
2026-09-07 14:27:01 +0300

    feat: add service structure

---
+++ analytics-service/config/config.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/caarlos0/env/v11"
+)
+
+type (
+	Config struct {
+		App  app  `envPrefix:"APP_"`
+		Http http `envPrefix:"HTTP_"`
+		Grpc grpc `envPrefix:"GRPC_"`
+		Pg   pg   `envPrefix:"PG_"`
+	}
+
+	app struct {
+		Name    string `env:"NAME,required"`
+		Version string `env:"VERSION,required"`
+	}
+
+	http struct {
+		Port string `env:"PORT,required"`
+	}
+
+	grpc struct {
+		Port string `env:"PORT,required"`
+	}
+
+	pg struct {
+		Host     string `env:"HOST,required"`
+		Port     int    `env:"PORT,required"`
+		User     string `env:"USER,required"`
+		Password string `env:"PASSWORD,required"`
+		DBName   string `env:"DBNAME,required"`
+		SSLMode  string `env:"SSLMODE,required"`
+	}
+)
+
+func (p pg) DSN() string {
+	return fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		p.Host, p.Port, p.User, p.Password, p.DBName, p.SSLMode)
+}
+
+func NewConfig() (*Config, error) {
+	cfg := &Config{}
+	if err := env.Parse(cfg); err != nil {
+		return nil, fmt.Errorf("config error: %w", err)
+	}
+	return cfg, nil
+}
---
+++ analytics-service/go.mod
@@ -0,0 +1,5 @@
+module analytics-service
+
+go 1.25.0
+
+require github.com/caarlos0/env/v11 v11.4.1
